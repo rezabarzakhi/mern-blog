b8209fbccaee0c8cc9d97978190b905229718363
Reza Barzakhi <reza.brzkhi@gmail.com>
2024-07-21 18:50:26 +0330

    connect to  database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
 
-const app = express()
+dotenv.config()
 
-app.listen(3003,()=>{
-    console.log("server running on port 3000sdsss");
-})
+mongoose
+  .connect(process.env.mongo)
+  .then(() => {
+    console.log("mongoDB is conncted");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+const app = express();
+
+app.listen(3000, () => {
+  console.log("server running on port 30003");
+});
