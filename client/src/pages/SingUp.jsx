e6a4bf087a576e681280b753d82574f3af3128c9
Reza Barzakhi <reza.brzkhi@gmail.com>
2024-07-23 11:19:10 +0330

    add functionalty to singup page

--- client/src/pages/SingUp.jsx
+++ client/src/pages/SingUp.jsx
@@ -1,8 +1,42 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import React from "react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SingUp() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate()
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage("Please fill out all fields");
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/singup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setLoading(false);
+        return setErrorMessage(data.message);
+      }
+      if (res.ok) {
+        navigate("/sing-in")
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -15,26 +49,33 @@ export default function SingUp() {
             Blog
           </Link>
           <p className="text-sm mt-5">
-            this is deom project, you can sinh up whit your email and password or with Google
+            this is demo project, you can sinh up whit your email and password or with Google
           </p>
         </div>
         {/* right */}
         <div className="flex-1">
-          <form className="flex flex-col gap-4 ">
+          <form className="flex flex-col gap-4 " onSubmit={handleSubmit}>
             <div>
               <Label value="your username" />
-              <TextInput type="text" placeholder="Username" id="username" />
+              <TextInput type="text" placeholder="Username" id="username" onChange={handleChange} />
             </div>
             <div>
               <Label value="your email" />
-              <TextInput type="text" placeholder="name@company.com" id="email" />
+              <TextInput type="email" placeholder="name@company.com" id="email" onChange={handleChange} />
             </div>
             <div>
               <Label value="your password" />
-              <TextInput type="text" placeholder="Password" id="password" />
+              <TextInput type="password" placeholder="Password" id="password" onChange={handleChange} />
             </div>
-            <Button gradientDuoTone="purpleToPink" type="submit">
-              Sing Up
+            <Button gradientDuoTone="purpleToPink" type="submit" disabled={loading}>
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">loading..</span>
+                </>
+              ) : (
+                "Sing Up"
+              )}
             </Button>
           </form>
           <div className="flex gap-2 text-sm mt-5">
@@ -43,6 +84,11 @@ export default function SingUp() {
               Sing In
             </Link>
           </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
